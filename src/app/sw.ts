50e9e275a36aeee021bb40cdcd0ec44917530b16
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-07-10 16:23:45 +0200

    Update service worker | not working

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,14 +1,51 @@
+import { MLCEngine, ServiceWorkerMLCEngineHandler } from "@mlc-ai/web-llm";
+import { init } from "next/dist/compiled/webpack/webpack";
 
 const CACHE_NAME = 'v1';
 const RESOURCES_TO_PRECACHE = [
   '/',
-  '/public',
-];
+  ];
 
-self.addEventListener("install", function (event) {
+let handler : ServiceWorkerMLCEngineHandler
+
+self.addEventListener('activate',(event) => {
+  
+  handler = new ServiceWorkerMLCEngineHandler();
+  console.log("Handelr")
+  console.log(handler)
+
+})
+
+self.addEventListener("install", function (event : any) {
     console.log("Hello world from the Service Worker 🤙");
 
-    // Pre-cache resources
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => {
+          console.log(cache)
+          return cache.addAll(RESOURCES_TO_PRECACHE);
+        }).catch((error) => {
+          console.log(error);
+        }
+        )
+    );
 
-    
 });
+self.addEventListener("fetch", function (event : any) {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener("message", (event : any) => {
+  console.log("Message")
+  console.log(event)
+
+})
