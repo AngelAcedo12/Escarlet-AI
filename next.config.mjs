b075ea02b4306a2fe2bb85c9a26851b8a2a1f909
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-07-12 20:42:16 +0200

    refactor: Add manifest link to head in layout.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next-pwa')} */
-const withPWA  = ({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-}); 
+
+
 
 const nextConfig = {
     
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,9 @@ export default function RootLayout({
   return (
 
     <html lang="esp" className=" text-white scroll-smooth h-full" >
-     
+     <head>
+      <link rel="manifest" href="/manifest.json" />
+     </head>
         
       <body  className={inter.className }  > 
       <ChatProvider>
